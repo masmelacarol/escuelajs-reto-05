d6ef81d3f7efda3df92ae0be93d1e96ffe01495d
masmelacarol <carolstefannym@gmail.com>
2020-07-13 16:51:37 -0500

    Challenge solved

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://rickandmortyapi.com/api/character/';
+let page = 1;
+let pages = 0;
 
 const getData = api => {
   fetch(api)
     .then(response => response.json())
     .then(response => {
       const characters = response.results;
+      pages = response.info.pages;
       let output = characters.map(character => {
         return `
       <article class="Card">
@@ -19,20 +22,47 @@ const getData = api => {
       newItem.classList.add('Items');
       newItem.innerHTML = output;
       $app.appendChild(newItem);
+      localStorage.setItem('next_fetch', JSON.stringify(characters));
     })
     .catch(error => console.log(error));
 }
 
-const loadData = () => {
-  getData(API);
+const loadData = async() => {
+  const dataLS = localStorage.getItem('next_fetch');
+  if(dataLS) {
+    await getData(`${API}/?page=${page}`);
+    page = page+1;
+  }else {
+    await getData(API);
+  } 
 }
 
-const intersectionObserver = new IntersectionObserver(entries => {
+const intersectionObserver = new IntersectionObserver((entries, observer) => {
   if (entries[0].isIntersecting) {
     loadData();
+    
+  } else if(page == pages){
+    observer.unobserve(entries[0].target);
+    $observe.classList.add('no-more-characters');
+    $observe.innerHTML = 'Ya no hay personajes...';
   }
+
+
 }, {
   rootMargin: '0px 0px 100% 0px',
+  threshold: 1.0
 });
 
-intersectionObserver.observe($observe);
+const handleVisibilityChange = () => {
+  const isVisible = document.visibilityState === "visible";
+  if (isVisible) {
+    if(localStorage.getItem('next_fetch')) {
+      intersectionObserver.observe($observe);
+    }
+  } else {
+    localStorage.clear()
+  }
+}
+
+
+handleVisibilityChange();
